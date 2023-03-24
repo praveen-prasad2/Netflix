fab1ec471d03966d4d96bd533a5304baa03bdd5c
Athira TU <aathuathira700@gmail.com>
2023-03-24 08:26:59 +0530

    completed fetching movie images

--- src/Components/List/List.jsx
+++ src/Components/List/List.jsx
@@ -1,65 +1,68 @@
-import React, { useEffect, useState } from 'react'
-import './List.css'
-import ArrowBackIosNewOutlinedIcon from '@mui/icons-material/ArrowBackIosNewOutlined';
-import ArrowForwardIosOutlinedIcon from '@mui/icons-material/ArrowForwardIosOutlined';
-import ListItem from '../Listitem/ListItem';
-import { useRef } from 'react';
-import {actionurl} from '../../API/API_KEY'
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import "./List.css";
+import ArrowBackIosNewOutlinedIcon from "@mui/icons-material/ArrowBackIosNewOutlined";
+import ArrowForwardIosOutlinedIcon from "@mui/icons-material/ArrowForwardIosOutlined";
+import ListItem from "../Listitem/ListItem";
+import { useRef } from "react";
+import { actionurl } from "../../API/API_KEY";
+import axios from "axios";
 
 function List() {
+  const [allMovie, setAllMovie] = useState();
 
-  const [allMovie,setAllMovie]=useState()
-
-async  function fetchMovie(){
-  const response=await axios.get(actionurl)
-  setAllMovie(response.data.results)
-    console.log(response)
+  async function fetchMovie() {
+    const response = await axios.get(actionurl);
+    setAllMovie(response.data.results);
+    // console.log(response);
   }
 
-  useEffect(()=>{fetchMovie()},[])
+  useEffect(() => {
+    fetchMovie();
+  }, []);
 
-  
-  const[isMoved,setIsMoved]=useState(false)
-  const[slideNumber,setslideNumber]=useState(0)
+  const [isMoved, setIsMoved] = useState(false);
+  const [slideNumber, setslideNumber] = useState(0);
 
-  const listRef=useRef()
+  const listRef = useRef();
 
-  const handleClick=(direction)=>{
-    setIsMoved(true)
-    let distance=listRef.current.getBoundingClientRect().x -50
-    if(direction=="left" && slideNumber>0){
-      setslideNumber(slideNumber-1)
-        listRef.current.style.transform=`translateX(${230+distance}px)`
-        console.log(distance);
+  const handleClick = (direction) => {
+    setIsMoved(true);
+    let distance = listRef.current.getBoundingClientRect().x - 50;
+    if (direction == "left" && slideNumber > 0) {
+      setslideNumber(slideNumber - 1);
+      listRef.current.style.transform = `translateX(${230 + distance}px)`;
+      console.log(distance);
     }
-    if(direction=="right" && slideNumber<5){
-      setslideNumber(slideNumber+1)
-        listRef.current.style.transform=`translateX(${-230+distance}px)`
-        console.log(distance);
+    if (direction == "right" && slideNumber < 5) {
+      setslideNumber(slideNumber + 1);
+      listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+      console.log(distance);
     }
-  }
+  };
   return (
-    <div className='list'>
-        <span className="listTitle">Continue to watch</span>
-        <div className="wrapper">
-          <ArrowBackIosNewOutlinedIcon className='sliderarrow left' onClick={()=>handleClick("left")} style={{display:!isMoved && "none"}}/>
-          <div className="container" ref={listRef}>
-            <ListItem index={0}/>
-            {
-              allMovie && allMovie.map((movie,key)=>{
-            return(
-            <ListItem index={key}/>
-            )
-
-              })
-            }
-          
-          </div>
-          <ArrowForwardIosOutlinedIcon className='sliderarrow right' onClick={()=>handleClick("right")}/>
+    <div className="list">
+      <span className="listTitle">Continue to watch</span>
+      <div className="wrapper">
+        <ArrowBackIosNewOutlinedIcon
+          className="sliderarrow left"
+          onClick={() => handleClick("left")}
+          style={{ display: !isMoved && "none" }}
+        />
+        <div className="container" ref={listRef}>
+         
+          {allMovie &&
+            allMovie.map((movie, key) => {
+              console.log(allMovie);
+              return <ListItem index={key} movie={movie} />;
+            })}
         </div>
+        <ArrowForwardIosOutlinedIcon
+          className="sliderarrow right"
+          onClick={() => handleClick("right")}
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default List
+export default List;
--- src/Components/Listitem/ListItem.jsx
+++ src/Components/Listitem/ListItem.jsx
@@ -5,23 +5,28 @@ import AddIcon from "@mui/icons-material/Add";
 import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
-function ListItem({ index }) {
+function ListItem({ index, movie }) {
   const [isHovered, setIsHovered] = useState(false);
   const trailer = "https://www.youtube.com/watch?v=b9EkMc79ZSU";
+  // console.log(movie);
   return (
     <div
       className="listitem"
-      style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
+      style={{
+        left: isHovered && index * 225 - 50 + index * 2.5,
+        background: `url(https://image.tmdb.org/t/p/original${movie.backdrop_path})`,backgroundSize:'cover'
+      }}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <img
+      {/* <img
         src="https://occ-0-2164-41.1.nflxso.net/dnm/api/v6/6gmvu2hxdfnQ55LZZjyzYR4kzGk/AAAABdTaFUt74e0yoFwDZ5X9qOIKr5tOFVksI4lrbXzP7P5pMkcoa4ALj4aqAhzOopDeJiLjHxjO_zdGflZV1GZRNdNNiJegwBGeu-0Iax5BLNol0aXNBH24-BpNc9nh9pow7RVi.jpg?r=92f"
         alt=""
-      />
+      /> */}
       {isHovered && (
         <>
           <div className="iteminfo">
+            <p>{movie.title}</p>
             <div className="icons">
               <PlayArrowIcon className="icon" />
               <AddIcon className="icon" />
